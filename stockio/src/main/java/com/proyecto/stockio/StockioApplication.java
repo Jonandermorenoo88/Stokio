8f3b90f2411991ca398bbf7917176e3e18324e24
jonandermorenoo88 <jaincera97@gmail.com>
2025-11-09 19:13:36 +0100

    Initial commit

---
+++ stockio/src/main/java/com/proyecto/stockio/StockioApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.stockio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockioApplication.class, args);
+	}
+
+}
---
+++ stockio/src/test/java/com/proyecto/stockio/StockioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.stockio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
